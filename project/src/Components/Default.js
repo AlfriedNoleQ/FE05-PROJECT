b37c284a2781fcf457ea579de90b3daa894d54b2
Thang Huu La <thanghuula@Thangs-MacBook-Pro.local>
2019-05-26 22:39:40 +0700

    Edit empty cart and 404 images

--- project/src/Components/Default.js
+++ project/src/Components/Default.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import pnf from '../images/404.png';
+import {Link} from 'react-router-dom';
+import pnf from '../images/404.gif';
 
 class Default extends Component {
     render() {
         return(
             <div className="container mt-5">
                 <div className="col-10 mx-auto text-center text-title">
-                    <img src={pnf} alt="404" height="350" width="550" />
+                    <img src={pnf} alt="404" height="300" width="450" />
+                    <h6 className="text-uppercase mt-2">Go back to <Link to="/">home</Link></h6>
                 </div>
             </div>
         )
